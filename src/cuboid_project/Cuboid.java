4a10007103c82d42324944df4dfd980bc33cf036
Anthony Gendron <aperturescience540@gmail.com>
2017-10-04 13:50:57 -0400

    Cuboid_Project

---
+++ src/cuboid_project/Cuboid.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cuboid_project;
+
+/**
+ *
+ * @author agendron
+ */
+public class Cuboid extends Cuboid_Project {
+    
+    private double length;
+    private double height;
+    private double width;
+    
+    
+    public Cuboid() {
+        
+    }
+    
+    public Double getLength()
+    {
+        return length;
+    }
+    
+    public Double getHeight()
+    {
+        return height;
+    }
+    
+    public Double getWidth()
+    {
+        return width;
+    }
+    
+    
+    
+    
+}
---
+++ src/cuboid_project/Cuboid_Project.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cuboid_project;
+
+/**
+ *
+ * @author agendron
+ */
+public class Cuboid_Project {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        //Cuboid<Double> c1 = new Cuboid<>(1.1, 2.2, 3.3);
+        
+        
+    }
+    
+}
